0773bd4b1a037f681147876a169e1ac1d5f6130f
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-27 19:28:38 +0100

    feat: add delete api

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ app.get('/api/courses', (req, res) => res.send(courses));
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(el => el.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Course Id is not Found!');
+    if (!course) return res.status(404).send('Course Id is not Found!');
     res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
     const { error } = validate(req.body);
-    if (error) res.status(400).send(error.message);
-    
+    if (error) return res.status(400).send(error.message);
+
     courses.push({ id: courses.length + 1, name: req.body.name });
     res.send(courses);
 });
 
 app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(el => el.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Course Id is not Found!');
+    if (!course) return res.status(404).send('Course Id is not Found!');
 
     const { error } = validate(req.body);
      if (error) res.status(400).send(error.message);
@@ -41,5 +41,14 @@ app.put('/api/courses/:id', (req, res) => {
      course.name = req.body.name;
      res.send(courses);
  });
+
+ app.delete('/api/courses/:id', (req, res) => {
+    const index = courses.findIndex(el => el.id === parseInt(req.params.id));
+    if (index < 0) return res.status(404).send('Course Id is not Found!');
+
+    courses.splice(index, 1);
+    
+    res.send(courses);
+});
 
 app.listen(port, () => console.log(`Listening to port ${port} ...`));
