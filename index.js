c39abaabbd2b119797e939f4d80912907aa8e158
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-27 18:44:35 +0100

    feat: add api post/add body parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
-const api = express();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -8,16 +12,21 @@ const courses = [
     { id: 3, name: 'course3' }
 ];
 
-api.get('/', (req, res) => res.send("Hello world!"));
+app.get('/', (req, res) => res.send("Hello world!"));
 
-api.get('/api/courses', (req, res) => res.send(courses));
+app.get('/api/courses', (req, res) => res.send(courses));
 
-api.get('/api/courses/:id', (req, res) => {
+app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(el => el.id === parseInt(req.params.id));
     if (!course) res.status(404).send('Course Id is not Found!');
     res.send(course);
 });
 
-const port = process.env.PORT || 3000;
+app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) res.status(400).send('Input is not valid!');
 
-api.listen(port, () => console.log(`Listening to port ${port} ...`));
+    courses.push({ id: courses.length + 1, name: req.body.name });
+    res.send(courses);
+});
+
+app.listen(port, () => console.log(`Listening to port ${port} ...`));
