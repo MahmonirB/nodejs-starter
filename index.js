2b644390ca52f293b17804e89c8ebbfd68e35c59
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-27 16:47:23 +0100

    feat: add api Get method

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const api = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 api.get('/', (req, res) => res.send("Hello world!"));
 
-api.get('/api/courses', (req, res) => res.send([1, 2, 3]));
+api.get('/api/courses', (req, res) => res.send(courses));
 
-api.listen(3000, () => console.log("Listening to port 3000 ..."));
+api.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(el => el.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course Id is not Found!');
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+
+api.listen(port, () => console.log(`Listening to port ${port} ...`));
