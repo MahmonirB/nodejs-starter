518e7af476d5fa832299db4f3bb9901521682282
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-29 17:54:25 +0100

    feat: Add regex, logical and comparison operand

--- dbQuery.js
+++ dbQuery.js
@@ -1,15 +1,16 @@
-const Course = require('./db');
+const Course = require("./db");
 
 async function getQuery() {
-  return await Course.find({ author: { $in: ["mosh"] } })
+  return await Course.find({ author: { $in: ["mosh"] }, name: /.*angular.*/i })
+    .or([{ tags: "backend" }, { tags: "frontend" }])
     .limit(10)
     .sort({ name: 1 })
-    .select({ name: 1, author: 1 });
+    .select({ name: 1, author: 1, tags: 1 });
 }
 
 async function displayQuery() {
-    const result = await getQuery();
-    console.log(result);
+  const result = await getQuery();
+  console.log(result);
 }
 
 module.exports = displayQuery;
