9439d044488c9101810e4a7312c869d0f1c8c949
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-29 18:27:52 +0100

    feat: Add update directly and option to show updated record

--- dbUpdateRecord.js
+++ dbUpdateRecord.js
@@ -3,14 +3,32 @@ const Course = require('./db');
 async function updateRecord(id) {
     const course = await Course.findById(id);
 
+    if(!course) return;
+
     course.author = "Mahi joon";
 
     return await course.save();
 
 }
 
+async function updateRecordDirectly(id) {
+    return await Course.update({ _id: id }, {
+        $set: {
+            author: 'Mahak'
+        }
+    });
+}
+
+async function updateRecordDirectlyWithRes(id) {
+    return await Course.findByIdAndUpdate(id, {
+        $set: {
+            author: 'Mahmonir khanum'
+        }
+    }, { new: 1 });
+}
+
 async function displayUpdated(id) {
-    const result = await updateRecord(id);
+    const result = await updateRecordDirectlyWithRes(id);
     console.log(result);
 }
 
