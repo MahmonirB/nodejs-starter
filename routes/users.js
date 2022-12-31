ad24b8af84e96df70765d74ffa6591ca9ece2335
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-31 20:48:59 +0100

    feat: users

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const User = require("../models/user");
+const { getHashedValue } = require("../hash");
+const { validateUser } = require("../validate");
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const isValid = validateUser(req.body);
+  if (isValid.error)
+    return res.status(400).send(isValid.error.details[0].message);
+
+  let user;
+  try {
+    user = await User.findOne({ email: req.body.email });
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  if (user) return res.status(400).send("User is registered before!");
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  user.password = await getHashedValue(user.password);
+
+  try {
+    const result = await user.save();
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  const token = jwt.sign({ id: user._id }, config.get("jwtPrivateKey"));
+  res.header('x-auth-token', token).send(_.pick(user, ["_id", "name", "email"]));
+});
+
+module.exports = router;
