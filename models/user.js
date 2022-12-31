67495c97d46a41044cc7ced716583b87e1637e4a
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2022-12-31 21:34:19 +0100

    feat: encapsulate token into response header (#4)

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 mongoose.set("strictQuery", false);
 
@@ -7,29 +9,33 @@ mongoose
   .then(() => console.log("Connected"))
   .catch((err) => console.log(err));
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      require: true,
-      minLength: 5,
-      maxLength: 255,
-    },
-    email: {
-      type: String,
-      require: true,
-      minLength: 5,
-      maxLength: 255,
-      unique: true,
-    },
-    password: {
-      type: String,
-      require: true,
-      minLength: 8,
-      maxLength: 255,
-    }
-  })
-);
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minLength: 5,
+    maxLength: 255,
+  },
+  email: {
+    type: String,
+    require: true,
+    minLength: 5,
+    maxLength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: 8,
+    maxLength: 255,
+  },
+});
+
+// Encapsulate generating token into user object, using user with this._id
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, config.get("jwtPrivateKey")); // get key from environment
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
