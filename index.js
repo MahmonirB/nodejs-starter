30959f72a4d0c24899ed0ae7f7e77680aea8c813
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-01-02 17:59:29 +0100

    feat: restructuring config

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const morgan = require("morgan");
 const pug = require("pug");
 const debug = require("debug")("app:startup"); // export DEBUG=app:startup,app:db or DEBUG=app:startup or DEBUG=app:*
 // const dbDebugger = require('debug')('app:db');
-const logger = require("./middleware/logger");
-const config = require("config");
-const dbPagination = require("./dbPagination");
-const dbUpdate = require("./dbUpdateRecord");
-const dbNewRecord = require("./dbNewRecord");
 const errorHandling = require("./middleware/errorHandling");
 const app = express();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/logging')();
+require('./startup/config')();
 
 const port = process.env.PORT || 3000;
 
@@ -26,14 +22,7 @@ if (app.get("env") === "development") {
   app.use(morgan("tiny"));
 }
 
-if (!config.get("jwtPrivateKey")) {
-  console.log("FATAL ERROR: jwt key not found!"); // export jwtPrivateKey=xxx and unset jwtPrivateKey=xxx
-  return process.exit(1);
-}
-
 app.use(helmet());
 app.use(errorHandling);
-
-app.use(logger); // custom middleware function
 
 app.listen(port, () => console.log(`Listening to port ${port} ...`));
